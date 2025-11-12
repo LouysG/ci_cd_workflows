2ab314bd1618c7d06e84d3ff84fb996d1b244674
Louys Gackstetter <louys.gackstetter@gmail.com>
2025-11-11 19:25:58 -0700

    Initial Commit

---
+++ src/my_package/my_code.py
@@ -0,0 +1,9 @@
+def my_function(x, y):
+    return x * y
+
+
+if __name__ == "__main__":
+    x = 5
+    y = 3
+    z = my_function(x, y)
+    print(z)
---
+++ tests/test_my_code.py
@@ -0,0 +1,5 @@
+from my_package.my_code import my_function
+
+
+def test_my_function():
+    assert my_function(5, 3) == 15
